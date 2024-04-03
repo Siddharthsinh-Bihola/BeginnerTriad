63b3c802e2b4aaa0c9259caf118adc3292fb7f4f
Siddharthsinh-Bihola <siddharthsinhbihola90@gmail.com>
2024-04-03 14:31:00 +0530

    Create Problem_2.c

---
+++ C Language/Formula_Based_Programs/Problem_2.c
@@ -0,0 +1,12 @@
+//Write a program to take an input of two integer numbers and print the sum of that numbers.
+//@Siddharthsinh-Bihola
+
+#include<stdio.h>
+
+int main(){
+
+    int a,b;    
+    printf("Enter two numbers:\n");
+    scanf("%d %d",&a,&b);
+    printf("The sum of two numbers %d and %d is %d.",a,b,a+b);
+}
