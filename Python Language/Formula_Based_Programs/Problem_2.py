d9daaa47b01c85831ec34bfad5a8426e51aaf514
Siddharthsinh-Bihola <siddharthsinhbihola90@gmail.com>
2024-04-08 21:06:44 +0530

    Create Problem_2.py

---
+++ Python Language/Formula_Based_Programs/Problem_2.py
@@ -0,0 +1,7 @@
+#Write a program to take an input of two integer numbers and print the sum of that numbers.
+#@Siddharthsinh_Bihola
+
+a=int(input("Enter the first number:"))
+b=int(input("Enter the Second number:"))
+
+print("The sum of",a,"and",b,"is",a+b,end=".")
